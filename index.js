8f27f2143c4be12c78d91ad56d9d7c06e9253f91
vaansuit <vvansuit@outlook.com>
2022-03-29 17:46:38 -0300

    update

---
+++ index.js
@@ -0,0 +1,114 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+let lastKey 
+canvas.width= 1024
+canvas.height= 570
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.2
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 150
+    }
+
+    draw() {
+        c.fillStyle ='blue'
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update(){
+        this.draw()
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if (this,this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else this.velocity.y += gravity
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0,
+    } 
+})
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0,
+    }
+})
+
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }, 
+    w: {
+        pressed: false
+    }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    //console.log('sad')
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+
+    player.velocity.x = 0
+
+    if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -1
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 1
+    }
+}
+
+animate()
+
+addEventListener('keydown', (event) => {
+    switch(event.key) {
+        case 'd': 
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 'w':
+            player.velocity.y = -10
+            break
+    }
+    //console.log(event.key)
+})
+
+addEventListener('keyup', (event) => { 
+    switch(event.key) {
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+    }
+})
